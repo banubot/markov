938284767359818a59b0963c4ea54f934fdb0b96
Hanny <falsemoo@gmail.com>
2019-02-01 01:58:53 -0700

    create map

--- markov.scala
+++ markov.scala
@@ -1,6 +1,8 @@
 import scala.io.Source
 import scala.io.StdIn
+import scala.collection.mutable.Map
 
+//get the filename and open
 def filename() = {
   if (args.length < 1) {
     StdIn.readLine("Please enter a file name: ")
@@ -8,12 +10,27 @@ def filename() = {
     args(0)
   }
 }
-
 val src = Source.fromFile(filename)
 
-for (line <- src.getLines) {
-  //TODO process line
-  println(line)
+var prefix1 = ""
+var prefix2 = ""
+
+/* create pairing of two words added together (prefix size 2) 
+ * map with param
+ * TODO word pair already exists in map
+ */
+def buildMap (word: String): (String, String) = {
+  var mapPiece = ((prefix1 + " " + prefix2) -> word)
+  prefix1 = prefix2
+  prefix2 = word
+  mapPiece
 }
 
+//create markov chain map from all words in file 
+val mappy = Map[String, String]()
+src.getLines foreach (line => 
+    line.split("\\s+") foreach (word =>
+        mappy += buildMap(word) )) 
+
+println(mappy)
 src.close
