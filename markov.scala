2ca8e8dfe60f4888f7e6d83de26300f081c7bf99
Hanny <falsemoo@gmail.com>
2019-02-20 21:54:53 -0700

    word exists in map

--- markov.scala
+++ markov.scala
@@ -1,36 +1,42 @@
 import scala.io.Source
 import scala.io.StdIn
 import scala.collection.mutable.Map
+import scala.collection.mutable.ArrayBuffer
 
 //get the filename and open
-def filename() = {
+val src = Source.fromFile({
   if (args.length < 1) {
     StdIn.readLine("Please enter a file name: ")
   } else {
     args(0)
   }
-}
-val src = Source.fromFile(filename)
+})
 
-var prefix1 = ""
-var prefix2 = ""
-
-/* create pairing of two words added together (prefix size 2) 
- * map with param
- * TODO word pair already exists in map
- */
-def buildMap (word: String): (String, String) = {
-  var mapPiece = ((prefix1 + " " + prefix2) -> word)
-  prefix1 = prefix2
-  prefix2 = word
-  mapPiece
+//get how many words to generate at most
+val maxOutLen = {
+  if (args.length < 2) {
+    StdIn.readLine("Please enter maximum number of words to output: ").toInt
+  } else {
+    args(1).toInt
+  }
 }
 
 //create markov chain map from all words in file 
-val mappy = Map[String, String]()
+val mappy = Map[ArrayBuffer[String], ArrayBuffer[String]]()
+var prefix1 = ""
+var prefix2 = ""
 src.getLines foreach (line => 
-    line.split("\\s+") foreach (word =>
-        mappy += buildMap(word) )) 
+    line.split("\\s+") foreach (word => {
+      val prefixes: ArrayBuffer[String] = ArrayBuffer(prefix1, prefix2)
+      if (mappy.contains(prefixes)) {
+        mappy(prefixes).append(word)
+      } else {
+        mappy += (prefixes -> ArrayBuffer(word))  
+      } 
+      prefix1 = prefix2
+      prefix2 = word
+    }))
 
+//TODO generate rando output
 println(mappy)
 src.close
